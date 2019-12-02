06f528d70dcdc64fa16722ca6198f3a0bbb542f9
Mark Said Camilleri <markscamilleri@users.noreply.github.com>
2019-12-02 12:29:54 +0000

    Fixed bugs in the SQLQueue code

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,63 +1,57 @@
 import logging
 import asyncio
-import queue
 import re
+from threading import Thread
 from typing import Optional, Callable, Dict, Any, List
 
-import mysql.connector as mysql
+import janus
+import mysql.connector
 import mysql.connector.pooling
-from mysql.connector import PoolError
 
-logger = logging.getLogger()
+from backend.src.exceptions import InvalidArgumentException, ProgramClosingException, SingletonException
 
 POOL_SIZE = 5
 
 
-class InvalidArgumentException(Exception):
-    pass
-
-
-class ProgramClosingException(Exception):
-    pass
-
-
-class SingletonException(Exception):
-    pass
-
-
 class SQLQueue:
-    _instances = {}
+    __instances = {}
 
     @staticmethod
     def get_instance(**database_args) -> 'SQLQueue':
         """ Static access method. """
-        if database_args not in SQLQueue._instances:
-            SQLQueue(database_args)
-        return SQLQueue.__instances[database_args]
+        if str(database_args) not in SQLQueue.__instances.keys():
+            SQLQueue(**database_args)
+        return SQLQueue.__instances[str(database_args)]
 
-    def __init__(self, database_args) -> 'SQLQueue':
+    def __init__(self, **database_args) -> 'SQLQueue':
         """ Virtually private constructor. """
-        if database_args not in SQLQueue.__instances:  # TODO Check this properly
+        if str(database_args) in SQLQueue.__instances.keys():
             raise SingletonException("This class is a singleton! Please use get_instance()")
         else:
             self.__database_args = database_args
-            self.__immediate_connection = mysql.connect(database_args)
-            self.__other_connection_pool = mysql.pooling.MySQLConnectionPool(pool_name="other_queries",
+            self.__immediate_connection = mysql.connector.connect(**database_args)
+            self.__other_connection_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="other_queries",
                                                                              pool_size=POOL_SIZE,
                                                                              **database_args)
             self.__other_connections_open = 0
-            self.__async_query_queue = asyncio.Queue()
             self.__async_query_queue_accepting = True
             self.__async_query_queue_runner_running = True
 
             self.__consumers = []
             for i in range(POOL_SIZE):
-                self.__consumers.append(asyncio.create_task(self.__query_queue_consumer()))
+                self.__consumers.append(self.__query_queue_consumer())
 
-            for i in range(self.__other_connection_pool.pool_size):
-                self.__other_connection_pool.add_connection()
+            self.__async_query_loop = asyncio.new_event_loop()
+            self.__query_queue = janus.Queue(loop=self.__async_query_loop)
+            self.__async_thread = Thread(target=SQLQueue.__start_loop, args=(self.__async_query_loop, self.__consumers))
+            self.__async_thread.start()
 
-            SQLQueue.__instances[database_args] = self
+            SQLQueue.__instances[str(database_args)] = self
+
+    @staticmethod
+    def __start_loop(loop, consumers):
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(asyncio.gather(*consumers))
 
     def select(self, query: str, parameters, fetch_all: bool = True) -> Dict[str, Any]:
         """
@@ -88,7 +82,7 @@ class SQLQueue:
         :return: Nothing
         """
         if self.__async_query_queue_accepting:
-            self.__async_query_queue.put({'query': query, 'parameters': parameters, 'callback': callback})
+            self.__query_queue.sync_q.put_nowait({'query': query, 'parameters': parameters, 'callback': callback})
         else:
             raise ProgramClosingException("The queue has closed")
 
@@ -96,6 +90,7 @@ class SQLQueue:
         """
         Blocking call
         """
+        logger = logging.getLogger(__name__)
         logging.debug(f"execute_with_result: query: {query}, parameters: {parameters}")
         cursor = self.__immediate_connection.cursor()
         logger.debug(f"Executing the query {query['query']} with parameters {query['parameters']} ")
@@ -107,32 +102,40 @@ class SQLQueue:
 
     async def __query_queue_consumer(self):
         # Waits until there's a free connection
+        logger = logging.getLogger(__name__)
+
         while self.__async_query_queue_runner_running:
-            try:
-                query = self.__async_query_queue.get()
-                id = hash((query['query'], query['parameters'], query['callback']))
-                logger.debug(f"{id}: Executing the query {query['query']} with parameters {query['parameters']} ")
+            query = await self.__query_queue.async_q.get()
+            query_hash = hash((query['query'], query['parameters'], query['callback']))
+            logger.debug(
+                f"{query_hash}: Executing the query {query['query']} with parameters {query['parameters']}")
 
-                self.__other_connections_open += 1
-                connection = self.__other_connection_pool.get_connection()
+            self.__other_connections_open += 1
+            connection = self.__other_connection_pool.get_connection()
 
-                cursor = connection.cursor()
-                cursor.execute(query['query'], query['parameters'])
+            cursor = connection.cursor()
+            cursor.execute(query['query'], query['parameters'])
 
-                result = cursor.fetchall()
-                logger.debug(f"{id}: result: {result}")
-                query['callback'](result)
-                connection.commit()
-                connection.close()
+            result = cursor.fetchall()
+            logger.debug(f"{query_hash}: result: {result}")
+            connection.commit()
+            connection.close()
 
-                self.__other_connections_open -= 1
-            except queue.Empty:
-                pass
+            self.__other_connections_open -= 1
+            logger.debug(f"{query_hash}: Connection closed. Running callback")
+            query['callback'](result)
 
-            logger.debug(f"{id}: Finished processing")
+            logger.debug(f"{query_hash}: Finished processing")
 
     def __del__(self):
+        logger = logging.getLogger(__name__)
+
+        logger.info("Closing SQLQueue")
+        logger.debug("Not accepting new queries")
         self.__async_query_queue_accepting = False
+        logger.debug("Closing immediate connection")
         self.__immediate_connection.close()
-        self.__async_query_queue.join()
+        logger.info("Waiting for tasks to finish")
+        self.__query_queue.sync_q.join()
+        logger.debug("Terminating Consumers")
         self.__async_query_queue_runner_running = False
