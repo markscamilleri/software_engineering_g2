a9d6ed9d33b23abc5beea105eb3535bd56e6ca1d
Kaylesh <56653205+Kaylesh@users.noreply.github.com>
2019-12-04 13:47:09 +0000

    New postcodeIO
    + Added 'f' string instead of deprecated format
    + Added change on develop string
    - Removed debugging print statements

--- postcodeIO.py
+++ postcodeIO.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, jsonify 
-import postcodes_io_api 
-import database 
-app = Flask(__name__) 
-@app.route('/getHouses', methods=['POST']) 
+from flask import Flask, request, jsonify
+import postcodes_io_api
+import database
+
+app = Flask(__name__)
+
+
+@app.route('/getHouses', methods=['POST'])
 def postcodesIO():
     frontData = request.get_json()
     latitude = frontData['lat']
@@ -11,28 +14,36 @@ def postcodesIO():
     houseLimit = frontData['limit']
     listOfPostcodes = callAPI(latitude, longitude, houseLimit, radius)
     # houseLimit used here to limit len(listOfPostcodes) amount of values in the SQL WHERE clause.
-    
+    varseq = ','.join(['%s']*len(listOfPostcodes))
+
     statement = (
-	"""SELECT post.id, house.paon, house.saon, post.street, post.postcode, props.initial, trans.price FROM postcodes AS post INNER JOIN houses AS house ON house.postcode_id = post.id INNER JOIN transactions AS trans ON trans.house_id = 
-house.id INNER JOIN property_types AS props ON props.id = house.property_type_id WHERE post.postcode IN ({seq});""".format(seq=','.join(['%s']*len(listOfPostcodes)))
-	)
-    print(statement)
-    print(str(listOfPostcodes))
+        f"""SELECT post.id, house.paon, house.saon, post.street, post.postcode, props.initial, trans.price 
+	FROM postcodes AS post 
+	INNER JOIN houses AS house 
+	ON house.postcode_id = post.id 
+	INNER JOIN transactions AS trans 
+	ON trans.house_id = house.id 
+	INNER JOIN property_types AS props 
+	ON props.id = house.property_type_id 
+	WHERE post.postcode IN ({varseq});"""
+    )
+
     result = db.select(query=statement, parameters=listOfPostcodes)
-    # --py-pseudo: for postcode in listOfPostcodes not necessarily a for loop --sql-pseudo: SELECT (postcode, id, street, county) FROM postcodes WHERE postcode IN str(tuple(listOfPostcodes)) SELECT (paon, saon) FROM houses WHERE houses.id = 
-    # postcodes.id (JOIN. DOUBLE CAUTION.) SELECT price FROM transactions WHERE transactions.id = houses.id (DOUBLE JOIN. TRIPLE CAUTION.) SELECT initial FROM property_types WHERE property_types.id = houses.id (If you don't get it now..)
-    print(str(result))
-    return jsonify(result) 
+
+    return jsonify(result)
+
 
 def callAPI(lat, lon, rad, lim):
     api = postcodes_io_api.Api(debug_http=True)
-    listPostcodes = api.get_nearest_postcodes_for_coordinates(latitude=lat, longitude=lon, limit=lim, radius=rad)
+    listPostcodes = api.get_nearest_postcodes_for_coordinates(
+        latitude=lat, longitude=lon, limit=lim, radius=rad)
     onlyPostcodes = []
     for i in range(len(listPostcodes["result"])):
-        print(str(i))
         onlyPostcodes.append(listPostcodes["result"][i]["postcode"])
-    return onlyPostcodes 
+    return onlyPostcodes
+
 
 if __name__ == '__main__':
-    db = database.SQLQueue.get_instance(host="34.89.126.252", user="root", password="GTFO", database="price_paid_data")
+    db = database.SQLQueue.get_instance(
+        host="34.89.126.252", user="root", password={change on develop}, database="price_paid_data")
     app.run(host='0.0.0.0', port=80)
